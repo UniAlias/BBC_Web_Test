f8108ae5cb0d7137dc4a1447f90c127bbd336e08
Ryan McConnell <ryanmc@ryans-mbp.home>
2019-02-22 02:44:03 +0000

    Ranking system in place.

--- homepage.js
+++ homepage.js
@@ -10,6 +10,8 @@ $('#pageNumber').text(pageNumber);
 //$('#articleContainer').text(pageNumber);
 var mainContainer = $('#bigBackground');
 
+
+//As soon as the page loads, get the json data for the first article
 $.ajax({
     type: "GET",
     url: "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-1.json",
@@ -18,6 +20,44 @@ $.ajax({
     success: function (response) {
         //print response
         console.log(response);
+
+        //This is an array that contains the data that we actually want to use
+        var usableData = response.body;
+
+        //loop through the data array
+        for (var i in usableData) {
+          console.log(usableData[i].type);
+          //Check if its a heading element and append appropriately
+          if (usableData[i].type == "heading") {
+            $('#articleDiv').append('<h3>' + usableData[i].model.text + '</h3');
+          } else if (usableData[i].type == "image") {
+            console.log(usableData[i].model.altText);
+            $('#articleDiv').append('<div><img src=' + usableData[i].model.url + ' alt="' + usableData[i].model.altText + '"></img></div>');
+          } else if (usableData[i].type == "paragraph") {
+            $('#articleDiv').append('<div><p>' + usableData[i].model.text + '</p></div>');
+          } else if (usableData[i].type == "list") {
+            //console.log("this is a list");
+            //console.log(usableData[i].model.type);
+            if (usableData[i].model.type == "unordered") {
+              var unorderedListOutput = "<div><ul>";
+              console.log(usableData[i].model.items);
+              for (var j in usableData[i].model.items) {
+                unorderedListOutput += "<li>" + usableData[i].model.items[j] + "</li>";
+              }
+              unorderedListOutput += "</ul></div>";
+              $('#articleDiv').append(unorderedListOutput);
+            } else if (usableData[i].model.type == "ordered") {
+              var orderedListOutput = "<div><ol>";
+              //console.log(usableData[i].model.items);
+              for (var k in usableData[i].model.items) {
+                orderedListOutput += "<li>" + usableData[i].model.items[k] + "</li>";
+              }
+              orderedListOutput += "</ol></div>";
+              $('#articleDiv').append(orderedListOutput);
+            }
+          }
+
+        }
 
     },
     error: function (xhr, status) {
@@ -39,6 +79,7 @@ $('#previousButton').click(function() {
 
   //Check that the pageNumber is within boundaries
   if ((pageNumber > 1) && (pageNumber < 6)) {
+
     //Wipe the articleContainer
     $('#articleContainer').empty();
     $('#articleContainer').text("");
@@ -95,6 +136,9 @@ $('#previousButton').click(function() {
 
         //Testing purposes
         pageNumber--;
+        if (pageNumber<5) {
+          $('#rateArticlesButton').hide();
+        }
         //$('#articleContainer').text(pageNumber);
         $('#pageNumber').text(pageNumber);
       },
@@ -195,4 +239,62 @@ $('#nextButton').click(function() {
   }
 });
 
+/* ===============================RATE ARTICLES BUTTON============================== */
+  $('#rateArticlesButton').click(function () {
+    //empty the articleDiv
+    $('#articleDiv').empty();
+
+  //   //Create the layout
+  //   $('#articleDiv').append('<div id = articleHeadings></div><div id = ratingScores></div>');
+  //
+  //   for (var i = 0; i < 5; i++) {
+  //     $.ajax({
+  //       type: "GET",
+  //       url: "https://raw.githubusercontent.com/bbc/news-coding-test-dataset/master/data/article-" + i + ".json",
+  //       dataType: 'json',
+  //       withCredentials: true,
+  //       success: function (response) {
+  //         //This is an array that contains the data that we actually want to use
+  //         var usableData = response.body;
+  //
+  //         //loop through the data array
+  //         for (var i in usableData) {
+  //           console.log(usableData[i].type);
+  //           //Check if its a heading element and append appropriately
+  //           if (usableData[i].type == "heading") {
+  //             $('#articleDiv').append('<h3>' + usableData[i].model.text + '</h3></div>');
+  //           }
+  //         }
+  //
+  //       }
+  //   });
+  //
+  // }
+
+//Creates the rating submission form
+  $('#articleDiv').append('<div><form id=formRank onsubmit="displayThanks()" method="post">The Highest Ranking Article<input type="text" name="1st"><br>2nd Highest Ranking Article<input type="text" name="2nd"><br>3rd Highest Ranking Article<input type="text" name="3rd"><br>4th Highest Ranking Article<input type="text" name="4th"><br>5th Highest Ranking Article<input type="text" name="5th"<br><br><input type="submit" value="Submit Rankings"></form></div>');
+
+
+
+
 });
+
+//Capture the form being submitted
+$('#formRank').on("submit",function(e) {
+    e.preventDefault(); // cancel the actual submit
+
+    //Get the articleDiv and empty it
+    $('#articleDiv').empty();
+
+    //Thank the user for the ranking
+    $('#articleDiv').append('<h2>Thank you for rating!</h2>')
+
+  });
+
+});
+
+function displayThanks()
+ {
+   $('#articleDiv').empty();
+   $('#articleDiv').append('<h2>Thank you for rating!</h2>')
+ }
